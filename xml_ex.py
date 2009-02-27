2c86b17092c335fa702d47100fd39dcd92bad292
Andrew Brehaut <andrew@brehaut.net>
2009-02-28 08:05:52 +1300

    text nodes, and entities

--- xml_ex.py
+++ xml_ex.py
@@ -28,11 +28,42 @@ close_angle = partial(one_of, '>')
 slash = partial(one_of, '/')
 equals = partial(one_of, '=')
 quote = partial(one_of, "\"'")
-element_text = compose(build_string, partial(many1, partial(not_one_of, ' \t\r\n<>/=')))
 whitespace = partial(many, partial(one_of, ' \t\n\r'))
+decimal_digit = partial(one_of, '0123456789')
+hex_decimal_digit = partial(one_of, '0123456789AaBbCcDdEeFf')
+
+def entity():
+    one_of('&')
+    ent = choice(named_entity, numeric_entity)
+    one_of(';')
+    return ent
+
+def named_entity():
+    name = build_string(many1(partial(not_one_of,';')))
+    if name not in named_entities: fail()
+    return named_entities[name]
+
+def hex_entity():
+    one_of('x')
+    return chr(int(build_string(many1(hex_decimal_digit)), 16))
+
+def dec_entity():
+    return chr(int(build_string(many1(hex_decimal_digit)), 16))
+    
+def numeric_entity():
+    one_of('#')
+    return choice(hex_entity, dec_entity)
+
+xml_char = partial(choice, partial(not_one_of, '<>&'), entity)
+
+element_text = compose(build_string, partial(many1, partial(not_one_of, ' \t\r\n<>/=')))
 
 def node():
-    return element()
+    return choice(element, text_node)
+
+def text_node():
+    text = build_string(many1(xml_char))
+    return "TEXT", text
 
 @tri
 def element():
@@ -77,11 +108,18 @@ def attribute():
     quote()
     return "ATTR", name, value
 
+    
+
 tokens, remaining = run_parser(node, """
-<xml version="1.0"><node foo="bar" baz="bop"><></node></xml>
+<xml version="1.0">
+    <node foo="bar" baz="bop">
+        this is some node text &amp; it contains an entity
+    </node>
+    <>
+</xml>
 """)
 
-print "Tokens:", tokens
+print "nodes:", tokens
 print
 print "remaining:", build_string(remaining)
 
