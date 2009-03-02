a034117e3d23f2215b5f2bea672b30b56343d919
steven.e.ashley@gmail.com <steven.e.ashley@gmail.com>
2009-03-02 14:57:46 +1300

    Docs

--- picoparse/__init__.py
+++ picoparse/__init__.py
@@ -356,6 +356,11 @@ def remaining():
     return tokens
 
 def seq(*sequence):
+    """Runs a series of parsers in sequence optionally storing results in a returned dictionary.
+    
+    For example:
+    seq(whitespace, ('phone', digits), whitespace, ('name', remaining))
+    """
     results = {}
     for p in sequence:
         if callable(p): 
