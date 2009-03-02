9a23fd1e75d0e8560581af144282fa8051abaf08
Andrew Brehaut <andrew@brehaut.net>
2009-03-02 13:08:59 +1300

    basic packaging setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='picoparse',
+      version='0.9',
+      description='Very small parser library for constructing parsers straight forward, and without regular expression',
+      author='Andrew Brehaut, Steven Ashley',
+      author_email='andrew@brehaut.net',
+      url='http://github.com/brehaut/picoparse/',
+      packages={'picoparse':'lib'}
+     )
