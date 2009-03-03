1fb99f3b8893702879e7cef6c49c68474d75b5f8
Matt Wilson <matt@culturethree.com>
2009-03-03 15:59:10 +1300

    hash-bang, and (probably pre-emptive) removal of some imports

--- examples/dice.py
+++ examples/dice.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
+"""A simple dice-expression parser."""
 from functools import partial
-from picoparse.text import build_string, caseless_string, quoted, quote, whitespace, whitespace1
+from picoparse.text import build_string, whitespace
 from picoparse import one_of, many, many1, not_one_of, run_parser, tri, commit, optional, fail
 from picoparse import choice, string, peek, cut, string, eof, many_until, any_token, satisfies
 from picoparse import sep, sep1, compose, cue
@@ -23,4 +25,5 @@ if __name__ == "__main__":
         EXP = sys.argv[1]
     else:
         EXP = "2D6+3"
+    
     print parse_exp(EXP)
