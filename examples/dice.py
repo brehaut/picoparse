6e189f7c433ad3b27a514bc20e818321bb7d52d3
Matt Wilson <matt@culturethree.com>
2009-03-03 15:55:00 +1300

    added dice expression parser to examples.

---
+++ examples/dice.py
@@ -0,0 +1,26 @@
+from functools import partial
+from picoparse.text import build_string, caseless_string, quoted, quote, whitespace, whitespace1
+from picoparse import one_of, many, many1, not_one_of, run_parser, tri, commit, optional, fail
+from picoparse import choice, string, peek, cut, string, eof, many_until, any_token, satisfies
+from picoparse import sep, sep1, compose, cue
+
+import sys
+
+decimal_digit = partial(one_of, '0123456789')
+
+number = compose(int, build_string)
+
+def dice_exp():
+    multiplier = number(many1(decimal_digit))
+    one_of("dD")
+    sides = number(many(decimal_digit)) or 6
+    return multiplier, "D", sides
+
+parse_exp = partial(run_parser, dice_exp)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        EXP = sys.argv[1]
+    else:
+        EXP = "2D6+3"
+    print parse_exp(EXP)
